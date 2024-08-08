1a96227b434ad2d171a37bd7bbe5999f8f4c3b29
= <=>
2024-08-08 10:12:30 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
 import React from 'react'
 import SavingsChart from './SavingsChart.jsx'
-import CustomRadioButtons from './CustomRadioButtons.jsx'
 
 function App() {
-
-// Example usage
-const options = [
-  { label: 'Option 1', value: '1' },
-  { label: 'Option 2', value: '2' },
-  { label: 'Option 3', value: '3' },
-];
+  const chartPieData = [
+    {
+      label: 'קרן פנסיה',
+      data: 501365,
+      backgroundColor: '#32CD32',
+      hoverBackgroundColor: '#3CB371',
+      tooltipColor: '#a1f5a2',
+    },
+    {
+      label: 'קופת גמל',
+      data: 400000,
+      backgroundColor: '#00008B',
+      hoverBackgroundColor: '#0000CD',
+      tooltipColor: '#9999ff',
+    },
+    {
+      label: 'גמל להשקעה',
+      data: 300000,
+      backgroundColor: '#4682B4',
+      hoverBackgroundColor: '#6495ED',
+      tooltipColor: '#b3d1e0',
+    },
+    {
+      label: 'קרן השתלמות',
+      data: 200000,
+      backgroundColor: '#87CEEB',
+      hoverBackgroundColor: '#87CEFA',
+      tooltipColor: '#c7e2f7',
+    },
+    {
+      label: 'ביטוח מנהלים',
+      data: 256470,
+      backgroundColor: '#FFD700',
+      hoverBackgroundColor: '#FFD700',
+      tooltipColor: '#ffff99',
+    },
+  ]
 
   return (
       <div>
-        jhjh
-        <CustomRadioButtons
-        options={options}
-        
-        />
-        {/* <SavingsChart /> */}
+        <SavingsChart data={chartPieData}/>
       </div>
   )
 }
--- src/SavingsChart.jsx
+++ src/SavingsChart.jsx
@@ -1,67 +1,89 @@
-import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const SavingsChart = () => {
-  const data = {
-    labels: ['קרן פנסיה', 'קופת גמל', 'גמל להשקעה', 'קרן השתלמות', 'ביטוח מנהלים'], 
+const SavingsChart = ({ data }) => {
+  const pieData = {
+    labels: data.map((item) => item.label),
     datasets: [
       {
-        data: [501365, 400000, 300000, 200000, 256470], // Example data
-        backgroundColor: ['#32CD32', '#00008B', '#4682B4', '#87CEEB', '#FFD700'],
-        hoverBackgroundColor: ['#32CD32', '#00008B', '#4682B4', '#87CEEB', '#FFD700'],
+        data: data.map((item) => item.data),
+        backgroundColor: data.map((item) => item.backgroundColor),
+        hoverBackgroundColor: data.map((item) => item.hoverBackgroundColor),
+        borderWidth: 3,
+        borderColor: "white",
+        hoverBorderColor: "white",
+        // hoverBorderWidth: 0, // need to custom here
+        borderRadius: 6, // need to custom here
+        
       },
     ],
   };
 
-  
   const options = {
     plugins: {
       legend: {
-        position: 'bottom',
+        position: "bottom",
         labels: {
           font: {
             size: 14,
-            family: 'calibri',
+            family: "calibri",
           },
-          padding: 10,
+          padding: 20,
           boxWidth: 20,
-          color: '#0f224b',
+          color: "#0f224b",
         },
       },
       tooltip: {
-        backgroundColor: '#ffffff', // Tooltip background color
-        titleColor: '#000000', // Tooltip title color
-        bodyColor: '#000000', // Tooltip body color
-        borderColor: '#0f224b', // Tooltip border color
+        backgroundColor: "#30e633", // Tooltip background color
+        titleColor: "#000000", // Tooltip title color
+        bodyColor: "#000000", // Tooltip body color
+        borderColor: "#0f224b", // Tooltip border color
         borderWidth: 1, // Tooltip border width
         titleFont: {
-          family: 'calibri',
+          family: "calibri",
           size: 16,
-          weight: 'bold',
+          weight: "bold",
         },
         bodyFont: {
-          family: 'calibri',
+          family: "calibri",
           size: 14,
         },
         callbacks: {
           label: function (tooltipItem) {
-            return tooltipItem.label + ': ₪' + tooltipItem.raw.toLocaleString();
+            return tooltipItem.label + ": ₪" + tooltipItem.raw.toLocaleString();
           },
         },
         padding: 10,
       },
     },
     responsive: true,
-    cutout: '70%', // To create the donut hole
+    cutout: "70%", // To create the donut hole
     maintainAspectRatio: false,
   };
 
   return (
-    <div style={{ position: 'relative', width: '50%', height: '50%' }}>
-      <Doughnut data={data} options={options} />
+    <div
+      style={{
+        background: "white",
+        boxSizing: "border-box",
+        width: "300px",
+        height: "310px",
+        padding: "0 0",
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "95%",
+          border: '0.5px solid black'
+        }}
+      >
+        <Doughnut data={pieData} options={options} />
+      </div>
     </div>
   );
 };
